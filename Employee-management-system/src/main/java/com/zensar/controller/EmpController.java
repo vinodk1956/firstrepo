5100ba302ef9f98ef32a144998e53ab5dcf86a72
vinodk1956 <101237167+vinodk1956@users.noreply.github.com>
2022-05-23 15:17:23 +0530

    Add files via upload

---
+++ Employee-management-system/src/main/java/com/zensar/controller/EmpController.java
@@ -0,0 +1,77 @@
+package com.zensar.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.zensar.entity.Employee;
+import com.zensar.service.EmpService;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+	private EmpService service;
+	
+	@GetMapping("/")
+	public String home(Model m)
+	
+	{
+		List<Employee> emp=service.getAllEmp();
+		m.addAttribute("emp",emp);
+		return "index";
+	}
+	
+	@GetMapping("/addemp")
+	public String addEmp()
+	{
+		return "add_emp";
+	}
+	
+	
+	@PostMapping("/register")
+	public String empRegister(@ModelAttribute Employee e,HttpSession session)
+	{
+		
+		System.out.println(e);
+		
+		service.addEmp(e);
+		
+		session.setAttribute("msg", "Employee Added Sucessfully...");
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id,Model m)
+	{
+		Employee e=service.getEmpById(id);
+		m.addAttribute("emp",e);
+		
+		return "edit";
+	}
+
+	@PostMapping("/update")
+	public String updateEmp(@ModelAttribute Employee e,HttpSession session)
+	{
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee Data Update Sucessfully..");
+		return "redirect:/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteEmp(@PathVariable int id,HttpSession session)
+	{
+		service.deleteEmp(id);
+		session.setAttribute("msg", "Employee Data Delete Sucessfully..");
+		return "redirect:/";
+	}
+}
